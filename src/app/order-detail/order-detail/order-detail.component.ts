7ebd278da0018b57c254925fb80efb606ea544d4
Rizwan <contact@fainin.com>
2023-07-09 01:38:17 +0500

    R-11: Grouped controls into a FormGroup

--- src/app/order-detail/order-detail/order-detail.component.ts
+++ src/app/order-detail/order-detail/order-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiObject} from "../services/order-detail.service";
 import {ActivatedRoute} from "@angular/router";
-import {FormControl, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {map, Observable, startWith} from "rxjs";
 
 interface Supplier {
@@ -14,7 +14,8 @@ interface Supplier {
   styleUrls: ['./order-detail.component.scss']
 })
 export class OrderDetailComponent implements OnInit {
-  supplierFormControl = new FormControl<string>('', [Validators.required]);
+  orderDetailsForm: FormGroup;
+  supplierControl = new FormControl<string>('', [Validators.required]);
   locationControl = new FormControl<string>('', [Validators.required]);
   supplierInvoice = new FormControl<string>('', [Validators.required, Validators.pattern('^[0-9]*$')]);
   orderNotes = new FormControl<string>('');
@@ -30,7 +31,13 @@ export class OrderDetailComponent implements OnInit {
   locations: string[] = ['default Location'];
   selectedLocation: string = '';
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder) {
+    this.orderDetailsForm = this.formBuilder.group({
+      supplier: this.supplierControl,
+      location: this.locationControl,
+      supplierInvoice: this.supplierInvoice,
+      notes: this.orderNotes,
+    })
   }
 
   ngOnInit() {
@@ -39,12 +46,12 @@ export class OrderDetailComponent implements OnInit {
      */
     ({products: this.products} = this.route.snapshot.data);
 
-    this.supplierFilteredOptions = this.supplierFormControl.valueChanges.pipe(
+    this.supplierFilteredOptions = this.supplierControl.valueChanges.pipe(
       startWith(''),
       map(value => {
         /**
          * Incorrect compile time error detection for type 'never' - could be potentially linked
-         * to some configuration within Angular 16. This didn't occur mid upgrades in Angular 15
+         * to some configuration within Angular 16. This didn't occur mid upgrades in Angular 14 and 15
          */
           // @ts-ignore
         const name = typeof value === 'string' ? value : value?.name;
