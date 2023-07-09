0885b3fae5a477d18c36fefeaab9522cf904fe69
Rizwan <contact@fainin.com>
2023-07-09 16:08:35 +0500

    R-16: Integrated Product Filtering Autocomplete

--- src/app/order-detail/order-detail/order-detail.component.ts
+++ src/app/order-detail/order-detail/order-detail.component.ts
@@ -4,7 +4,7 @@ import {ActivatedRoute} from "@angular/router";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {map, Observable, startWith} from "rxjs";
 
-interface Supplier {
+interface AutocompleteOptionsObject {
   name: string;
 }
 
@@ -21,11 +21,18 @@ export class OrderDetailComponent implements OnInit {
   orderNotes = new FormControl<string>('', [Validators.max(200)]);
 
   /**
+   * Only for autocomplete
+   */
+  productAutocompleteControl = new FormControl<string>('');
+
+  /**
    * Deduced from the type of dummy data being fetched
    */
-  supplierOptions: Supplier[] = [{name: 'Clothing Supplier'}, {name: 'Accessories Supplier'}, {name: 'Tech Supplier'}, {name: 'Fashion Supplier'}];
+  supplierOptions: AutocompleteOptionsObject[] = [{name: 'Clothing Supplier'}, {name: 'Accessories Supplier'}, {name: 'Tech Supplier'}, {name: 'Fashion Supplier'}];
+  productOptions: AutocompleteOptionsObject[] = [];
 
-  supplierFilteredOptions: Observable<Supplier[]> | undefined;
+  supplierFilteredOptions: Observable<AutocompleteOptionsObject[]> | undefined;
+  productFilteredOptions: Observable<AutocompleteOptionsObject[]> | undefined;
 
   products: ApiObject[] | undefined;
   locations: string[] = ['default Location'];
@@ -46,14 +53,34 @@ export class OrderDetailComponent implements OnInit {
      */
     ({products: this.products} = this.route.snapshot.data);
 
+    this.assignProductOptions(this.products);
+    this.assignProductFilteredOptions();
     this.assignSupplierFilteredOptions();
 
     console.warn("Getting product list from resolver:");
     console.warn(this.products);
   }
 
-  supplierDisplayFn(user: Supplier): string {
+  supplierDisplayFn(user: AutocompleteOptionsObject): string {
     return user && user.name ? user.name : '';
+  }
+
+  productDisplayFn(product: ApiObject): string {
+    if (product) {
+      return `${product.id} ${product.title}`;
+    } else {
+      return '';
+    }
+  }
+
+  private assignProductOptions(products: ApiObject[] | undefined) {
+    if (products) {
+      for (let a = 0; a < products.length; a++) {
+        this.productOptions.push({
+          name: `${products[a].id} ${products[a].title}`,
+        });
+      }
+    }
   }
 
   private assignSupplierFilteredOptions() {
@@ -71,7 +98,29 @@ export class OrderDetailComponent implements OnInit {
     );
   }
 
-  private _filterSupplier(name: string): Supplier[] {
+  private assignProductFilteredOptions() {
+    this.productFilteredOptions = this.productAutocompleteControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        /**
+         * Incorrect compile time error detection for type 'never' - could be potentially linked
+         * to some configuration within Angular 16. This didn't occur mid upgrades in Angular 14 and 15
+         */
+          // @ts-ignore
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filterProducts(name as string) : this.productOptions.slice();
+      }),
+    );
+  }
+
+
+  private _filterProducts(name: string): AutocompleteOptionsObject[] {
+    const filterValue = name.toLowerCase();
+
+    return this.productOptions.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+
+  private _filterSupplier(name: string): AutocompleteOptionsObject[] {
     const filterValue = name.toLowerCase();
 
     return this.supplierOptions.filter(option => option.name.toLowerCase().includes(filterValue));
