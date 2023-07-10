686af4bf3e0b9628a82b6441228aaf16998cd0c1
Rizwan <contact@fainin.com>
2023-07-10 15:50:43 +0500

    R-32: Fixed Karma config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rewaa-case-study app is running!');
+    expect(compiled.querySelector('.tab-bar')?.textContent).toContain('Rewaa Case Study');
   });
 });
