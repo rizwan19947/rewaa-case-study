223a645a1889ba872ee2aa5528c133e5d1b0c849
Rizwan <contact@fainin.com>
2023-07-10 17:36:06 +0500

    R-33: Added a test data model

--- src/app/order-detail/order-detail/order-detail.component.spec.ts
+++ src/app/order-detail/order-detail/order-detail.component.spec.ts
@@ -1,23 +1,86 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed, async} from '@angular/core/testing';
 
-import { OrderDetailComponent } from './order-detail.component';
+import {OrderDetailComponent} from './order-detail.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {MatAutocomplete} from "@angular/material/autocomplete";
+import {dummyData} from "../models/test-data.model";
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('OrderDetailComponent', () => {
   let component: OrderDetailComponent;
   let fixture: ComponentFixture<OrderDetailComponent>;
+  let el: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ OrderDetailComponent ]
+      imports: [RouterTestingModule],
+      declarations: [OrderDetailComponent, MatAutocomplete]
     })
-    .compileComponents();
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(OrderDetailComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+      });
 
-    fixture = TestBed.createComponent(OrderDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display product data table', () => {
+    component.products = dummyData;
+    fixture.detectChanges();
+
+    const productsTableContainer = el.queryAll(By.css('.product-table'));
+
+    expect(productsTableContainer).toBeTruthy('Product/Product List does not exist!');
+  });
+
+  it('should display prepaid container', () => {
+    component.products = dummyData;
+    component.paymentPlan = 'prepaid';
+    fixture.detectChanges();
+
+    const paymentContainer = el.queryAll(By.css('.prepaid-container'));
+
+    expect(paymentContainer).toBeTruthy('Incorrect payment container displayed!');
+  });
+
+  it('should display postpaid container', () => {
+    component.products = dummyData;
+    component.paymentPlan = 'postpaid';
+    fixture.detectChanges();
+
+    const paymentContainer = el.queryAll(By.css('.postpaid-container'));
+
+    expect(paymentContainer).toBeTruthy('Incorrect payment container displayed!');
+  });
+
+  it('submit button should be initially disabled', () => {
+    fixture.detectChanges();
+
+    const submitButton = el.query(By.css('#submit-button')).nativeElement as HTMLButtonElement;
+
+    expect(submitButton.disabled).toBeTruthy();
+  });
+
+  it('no products should be initially selected', () => {
+    component.products = dummyData;
+    fixture.detectChanges();
+
+    /**
+     * Checking selection by dom
+     */
+    expect(component.selectedProducts?.length).toBe(0, 'Not initially empty as expected');
+
+    /**
+     * Checking selection programmatically
+     */
+    expect(component.minimumProductsSelected()).toBe(false, 'Not initially empty as expected');
+  });
 });
+
+
